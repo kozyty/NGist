5d28fe0effc27e2bca93b7d815cb60c1dd8201ae
kozyty <taiyo.kojima@nanapi.co.jp>
2012-08-29 20:08:27 +0900

    debeso => NGist

--- app/models/setting.rb
+++ app/models/setting.rb
@@ -2,7 +2,7 @@ class Setting
   SETTING_PATH = File.dirname(__FILE__) + '/../../config/setting.yml'
   @@available_settings = File.exist?(SETTING_PATH) ?
                          YAML.load(File.open(SETTING_PATH)) :
-                         { 'repository_root' => File.dirname(__FILE__) + "/../../tmp/debeso"}
+                         { 'repository_root' => File.dirname(__FILE__) + "/../../tmp/ngist"}
   def self.[](key)
     @@available_settings[key.to_s]
   end
